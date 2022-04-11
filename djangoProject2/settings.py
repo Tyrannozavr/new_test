11ffed9e7b5210055ff274d2fafd457669427826
dmiv <dmitriy40647274@gmail.com>
2022-04-11 11:49:40 +0300

    test

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -135,6 +135,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = '/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
