2cc84d929a2ef58bbd3dbbe2a07004cbfe7b4252
dmiv <dmitriy40647274@gmail.com>
2022-04-11 17:24:22 +0300

    debug false

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-thepmpj5x+8d)@
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
