66c14a517a0d5d08810f50820c8e653d158be421
dmiv <dmitriy40647274@gmail.com>
2022-04-11 17:04:01 +0300

    опечатка

--- djangoProject2/views.py
+++ djangoProject2/views.py
@@ -45,10 +45,9 @@ def diagram_json(request):
         model = Ai_two
     else:
         return redirect('/')
-    a = model.objects.all()
+    a = model.objects.all().order_by('id')
     labels = [f"{i.id}" for i in a]
     values = [float(i.current) for i in a]
-    a.s
     # labels = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2323"]
     # values = [100, 320, 453, 234, 553, 665, 345, 123, 432, 545, 654, 345, 332, 456, 234]
     result = {
