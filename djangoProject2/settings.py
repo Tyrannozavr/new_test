362a875def76a93255f18944a5b41deaf369bdd3
dmiv <dmitriy40647274@gmail.com>
2022-04-11 11:17:15 +0300

    changed Procfile

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-thepmpj5x+8d)@
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
